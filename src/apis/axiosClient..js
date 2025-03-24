02aa5a7cea83d52af094a8f29b24fec148b5d9f6
NamProPlayer <nguyenthitrangg01011976@gmail.com>
2025-03-24 08:11:15 +0700

    change baseUrl to https://backendimg-rmha.onrender.com

--- src/apis/axiosClient..js
+++ src/apis/axiosClient..js
@@ -2,7 +2,7 @@ import axios from "axios";
 //http://localhost:8080
 //https://backendimg-rmha.onrender.com
 const axiosClient = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://backendimg-rmha.onrender.com",
     timeout: 5000,
     headers: {
         "Content-Type": "application/json"
